bbf05fdff72430fc8b5811d13fd3294ad5a7d7a0
Nick Tindall <nick@tindall.id.au>
2020-05-14 21:57:29 +1000

    Implement election scheduler

---
+++ src/main/java/au/id/tindall/distalg/raft/driver/ElectionScheduler.java
@@ -0,0 +1,86 @@
+package au.id.tindall.distalg.raft.driver;
+
+import au.id.tindall.distalg.raft.Server;
+import org.apache.logging.log4j.Logger;
+
+import java.io.Serializable;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.apache.logging.log4j.LogManager.getLogger;
+
+public class ElectionScheduler<ID extends Serializable> {
+
+    private static final Logger LOGGER = getLogger();
+
+    private Server<ID> server;
+    private final ElectionTimeoutGenerator electionTimeoutGenerator;
+    private final ScheduledExecutorService scheduledExecutorService;
+    private volatile ScheduledFuture<?> nextElectionTimeout;
+    private volatile boolean timeoutsRunning = false;
+
+    public ElectionScheduler(ElectionTimeoutGenerator electionTimeoutGenerator, ScheduledExecutorService scheduledExecutorService) {
+        this.electionTimeoutGenerator = electionTimeoutGenerator;
+        this.scheduledExecutorService = scheduledExecutorService;
+    }
+
+    public void setServer(Server<ID> server) {
+        this.server = server;
+    }
+
+    public synchronized void startTimeouts() {
+        LOGGER.debug("Starting election timeouts for server " + server.getId());
+        if (timeoutsRunning) {
+            throw new IllegalStateException("Timeouts are already started");
+        }
+        timeoutsRunning = true;
+        scheduleElectionTimeout();
+    }
+
+    public synchronized void resetTimeout() {
+        if (!timeoutsRunning) {
+            throw new IllegalStateException("Timeouts are not started");
+        }
+        cancelPendingTimeout();
+        scheduleElectionTimeout();
+    }
+
+    public synchronized void stopTimeouts() {
+        LOGGER.debug("Stopping election timeouts for server " + server.getId());
+        if (!timeoutsRunning) {
+            throw new IllegalStateException("Timeouts are not started");
+        }
+        timeoutsRunning = false;
+        cancelPendingTimeout();
+    }
+
+    private void cancelPendingTimeout() {
+        if (nextElectionTimeout != null) {
+            LOGGER.debug("Cancelling election timeout... (" + server.getId() + ")");
+            boolean cancel = nextElectionTimeout.cancel(false);
+            if (cancel) {
+                LOGGER.debug("Successfully cancelled timeout (" + server.getId() + ")");
+            } else {
+                LOGGER.debug("Couldn't cancel previous timeout (" + server.getId() + ")");
+            }
+            nextElectionTimeout = null;
+        }
+    }
+
+    private void scheduleElectionTimeout() {
+        Long next = electionTimeoutGenerator.next();
+        LOGGER.debug("Scheduling election timeout for " + next + " milliseconds (" + server.getId() + ")");
+        nextElectionTimeout = scheduledExecutorService.schedule(
+                this::performElectionTimeout,
+                next,
+                MILLISECONDS);
+    }
+
+    private synchronized void performElectionTimeout() {
+        LOGGER.debug("Election timeout occurred: server " + server.getId());
+        nextElectionTimeout = null;
+        server.electionTimeout();
+        resetTimeout();
+    }
+}
---
+++ src/test/java/au/id/tindall/distalg/raft/driver/ElectionSchedulerTest.java
@@ -0,0 +1,143 @@
+package au.id.tindall.distalg.raft.driver;
+
+import au.id.tindall.distalg.raft.Server;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ElectionSchedulerTest {
+
+    public static final long TIMEOUT_MILLIS = 12345L;
+    @Mock
+    private Server<Long> server;
+    @Mock
+    private ElectionTimeoutGenerator electionTimeoutGenerator;
+    @Mock
+    private ScheduledExecutorService scheduledExecutorService;
+    @Mock
+    private ScheduledFuture timeoutFuture;
+
+    private ElectionScheduler<Long> electionScheduler;
+
+    @BeforeEach
+    void setUp() {
+        electionScheduler = new ElectionScheduler<>(electionTimeoutGenerator, scheduledExecutorService);
+        electionScheduler.setServer(server);
+    }
+
+    @Nested
+    class StartElectionTimeouts {
+
+        @BeforeEach
+        void setUp() {
+            when(electionTimeoutGenerator.next()).thenReturn(TIMEOUT_MILLIS);
+            electionScheduler.startTimeouts();
+        }
+
+        @Test
+        void willScheduleAnElectionTimeout() {
+            verify(scheduledExecutorService).schedule(any(Runnable.class), eq(TIMEOUT_MILLIS), eq(MILLISECONDS));
+        }
+
+        @Test
+        void willThrowIfTimeoutsAlreadyStarted() {
+            assertThatThrownBy(() -> electionScheduler.startTimeouts()).isInstanceOf(IllegalStateException.class);
+            verify(scheduledExecutorService).schedule(any(Runnable.class), eq(TIMEOUT_MILLIS), eq(MILLISECONDS));
+        }
+    }
+
+    @Nested
+    class ResetElectionTimeout {
+
+        @Test
+        @SuppressWarnings("unchecked")
+        public void willCancelExistingTimeoutThenScheduleANewOne() {
+            when(electionTimeoutGenerator.next()).thenReturn(TIMEOUT_MILLIS);
+            when(scheduledExecutorService.schedule(any(Runnable.class), eq(TIMEOUT_MILLIS), eq(MILLISECONDS)))
+                    .thenReturn(timeoutFuture);
+
+            electionScheduler.startTimeouts();
+            reset(scheduledExecutorService);
+
+            electionScheduler.resetTimeout();
+            InOrder sequence = inOrder(timeoutFuture, scheduledExecutorService);
+            sequence.verify(timeoutFuture).cancel(false);
+            sequence.verify(scheduledExecutorService).schedule(any(Runnable.class), eq(TIMEOUT_MILLIS), eq(MILLISECONDS));
+        }
+
+        @Test
+        public void willThrowIfTimeoutsAreNotStarted() {
+            assertThatThrownBy(() -> electionScheduler.resetTimeout()).isInstanceOf(IllegalStateException.class);
+        }
+    }
+
+    @Nested
+    class StopElectionTimeouts {
+
+        @Test
+        @SuppressWarnings("unchecked")
+        void willCancelOutstandingTimeout() {
+            when(electionTimeoutGenerator.next()).thenReturn(TIMEOUT_MILLIS);
+            when(scheduledExecutorService.schedule(any(Runnable.class), eq(TIMEOUT_MILLIS), eq(MILLISECONDS)))
+                    .thenReturn(timeoutFuture);
+            electionScheduler.startTimeouts();
+
+            electionScheduler.stopTimeouts();
+            verify(timeoutFuture).cancel(false);
+        }
+
+
+        @Test
+        void willThrowWhenTimeoutsAreNotStarted() {
+            assertThatThrownBy(() -> electionScheduler.stopTimeouts()).isInstanceOf(IllegalStateException.class);
+        }
+    }
+
+    @Nested
+    class ElectionTimeout {
+
+        private Runnable timeoutFunction;
+
+        @BeforeEach
+        void setUp() {
+            when(electionTimeoutGenerator.next()).thenReturn(TIMEOUT_MILLIS);
+            when(scheduledExecutorService.schedule(any(Runnable.class), eq(TIMEOUT_MILLIS), eq(MILLISECONDS)))
+                    .thenReturn(timeoutFuture);
+            electionScheduler.startTimeouts();
+            ArgumentCaptor<Runnable> captor = ArgumentCaptor.forClass(Runnable.class);
+            verify(scheduledExecutorService).schedule(captor.capture(), eq(TIMEOUT_MILLIS), eq(MILLISECONDS));
+            timeoutFunction = captor.getValue();
+            reset(scheduledExecutorService);
+        }
+
+        @Test
+        void willCallElectionTimeoutOnTheServer() {
+            timeoutFunction.run();
+            verify(server).electionTimeout();
+        }
+
+        @Test
+        void willScheduleNextTimeout() {
+            timeoutFunction.run();
+            verify(scheduledExecutorService).schedule(any(Runnable.class), eq(TIMEOUT_MILLIS), eq(MILLISECONDS));
+        }
+    }
+}
